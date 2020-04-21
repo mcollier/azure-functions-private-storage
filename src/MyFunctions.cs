1f38d245772f612e7efd6f8cee65c02c7a77e1f7
Michael Collier <mcollier@microsoft.com>
2020-04-21 00:37:57 -0400

    Removed HTTP trigger; added Cosmos DB private endpoint

--- src/MyFunctions.cs
+++ src/MyFunctions.cs
@@ -12,32 +12,19 @@ namespace Company.Function
 {
     public static class MyFunctions
     {
-        [FunctionName("HttpTriggerCSharp1")]
-        public static async Task<IActionResult> HelloWorldFunction(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
-
         [FunctionName("QueueTrigger")]
-        public static void GetMessagesFunctions(
+        public static async Task GetMessagesFunctionsAsync(
                     [QueueTrigger("%QueueName%", Connection = "PrivateEndpointStorage")] string queueItem,
-                    ILogger logger)
+                    [CosmosDB(
+                        databaseName: "%CosmosDbName%",
+                        collectionName: "%CosmosDbCollectionName%",
+                        ConnectionStringSetting = "CosmosDBConnection")]
+                        IAsyncCollector<string> items,
+                        ILogger logger)
         {
             logger.LogInformation($"Queue trigger processed: {queueItem}");
+
+            await items.AddAsync(queueItem);
         }
     }
 }
--- template/deploy.sh
+++ template/deploy.sh
@@ -1,31 +1,81 @@
-#!/bin/bash -v
+#!/bin/bash
 
 #az login
 #az account set -s 
 
 
-resourceGroupName="function-private-storage-demo-scus-script-2"
+resourceGroupName="function-private-storage-demo-scus-10"
 location="southcentralus"
 now=`date +%Y%m%d-%H%M%S`
 deploymentName="azuredeploy-$now"
 
-
+echo "Creating resource group '$resourceGroupName' in region '$location' . . ."
 az group create --name $resourceGroupName --location southcentralus
 
+echo "Setting defaults ...."
 az configure --defaults group=$resourceGroupName location=$location
 
 # az deployment group validate --template-file azuredeploy.json --parameters azuredeploy.parameters.json --debug
 
+echo "Deploying main template . . ."
 az deployment group create --template-file azuredeploy.json --parameters azuredeploy.parameters.json --name $deploymentName
 
-storagePrivateNic=$(az deployment group show -g $resourceGroupName --name $deploymentName --query properties.outputs.privateEndpointNetworkInterface.value)
-echo $storagePrivateNic
 
-# Get the IP address
-storagePrivateIpAddress=$(az resource show --ids $storagePrivateNic --query properties.ipConfigurations[0].properties.privateIPAddress)
-echo $storagePrivateIpAddress
+# Get Azure Storage Private Endpoint data
+echo "Getting Azure Storage queue private endpoint . . ."
+storagePrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateEndpointNetworkInterface.value --output tsv)
+echo "Private storage queue NIC is $storagePrivateNic"
 
-# # Get the DNS record
-storagePrivateDnsRecord=$(az deployment group show -g $resourceGroupName --name $deploymentName --query properties.outputs.privateDNSRecordName.value)
 
-# az deployment group create -g function-private-storage-demo-scus --template-file PrivateZoneRecords_template.json --parameters DNSRecordName='[PRIVATE DNS NAME]' IPAddress='[PRIVATE IP ADDRESS]'
+if [[ ! -z $storagePrivateNic ]]; then
+
+    # Get the IP address
+    echo "Getting Azure Storage queue private IP address . . ."
+    storagePrivateIpAddress=$(az resource show --ids $storagePrivateNic --query properties.ipConfigurations[0].properties.privateIPAddress --output tsv)
+    echo $storagePrivateIpAddress
+
+    # # Get the DNS record
+    echo "Getting private storage DNS record . . ."
+    storagePrivateDnsRecord=$(az deployment group show --name $deploymentName --query properties.outputs.privateDNSRecordName.value --output tsv)
+
+    # Deploy private zone for storage
+    echo "Deploying private DNS zone records for storage . . . "
+    az deployment group create --template-file PrivateZoneRecords_template.json --parameters DNSRecordName=$storagePrivateDnsRecord IPAddress=$storagePrivateIpAddress
+
+fi
+
+# Get Azure Cosmos DB Private Endpoint data
+cosmosDbPrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateEndpointCosmosDbNetworkInterface.value --output tsv)
+echo "CosmosDB private NIC is" $cosmosDbPrivateNic
+
+if [[ ! -z $cosmosDbPrivateNic ]]; then
+
+    # Get the IP address
+    echo "Getting CosmosDB private IP address . . ."
+    cosmosDbPrivateIpAddress=$(az resource show --ids $cosmosDbPrivateNic --query properties.ipConfigurations[0].properties.privateIPAddress --output tsv)
+    echo $cosmosDbPrivateIpAddress
+
+    echo "Getting CosmosDB private member name . . . "
+    cosmosDbPrivateMemberName=$(az resource show --ids $cosmosDbPrivateNic --query properties.ipConfigurations[0].properties.privateLinkConnectionProperties.requiredMemberName --output tsv)
+
+
+     echo "Getting second CosmosDB private IP address . . ."
+    cosmosDbPrivateIpAddress2=$(az resource show --ids $cosmosDbPrivateNic --query properties.ipConfigurations[1].properties.privateIPAddress --output tsv)
+    echo $cosmosDbPrivateIpAddress2
+
+    echo "Getting second CosmosDB private member name . . . "
+    cosmosDbPrivateMemberName2=$(az resource show --ids $cosmosDbPrivateNic --query properties.ipConfigurations[1].properties.privateLinkConnectionProperties.requiredMemberName --output tsv)
+
+
+    # Get the DNS record
+    echo "Getting CosmosDB private DNS record . . ."
+    cosmosDbPrivateDnsRecord=$(az deployment group show --name $deploymentName --query properties.outputs.privateCosmosDbDNSRecordName.value --output tsv)
+
+## FIX UP THE HARD CODED BELOW
+
+    # Deploy private zone for Cosmos DB
+    echo "Deploying private DNS zone record for CosmosDB . . ."
+    az deployment group create --template-file PrivateZoneRecords_template.json --parameters DNSRecordName=$cosmosDbPrivateDnsRecord IPAddress=$cosmosDbPrivateIpAddress
+
+    az deployment group create --template-file PrivateZoneRecords_template.json --parameters DNSRecordName='privatelink.documents.azure.com/'$cosmosDbPrivateMemberName2 IPAddress=$cosmosDbPrivateIpAddress2
+fi

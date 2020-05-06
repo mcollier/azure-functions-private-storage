fb71fc726351fe74cf18ba1206b6e2aba1cf717e
Michael Collier <mcollier@microsoft.com>
2020-05-06 17:35:46 -0400

    Use new DNS zone groups; default to not deploy code.

--- template/deploy.sh
+++ template/deploy.sh
@@ -4,7 +4,7 @@
 #az account set -s 
 
 
-resourceGroupName="function-private-storage-601"
+resourceGroupName="function-private-storage-1002"
 location="southcentralus"
 now=`date +%Y%m%d-%H%M%S`
 deploymentName="azuredeploy-$now"
@@ -22,92 +22,3 @@ az configure --defaults group=$resourceGroupName location=$location
 
 echo "Deploying main template . . ."
 az deployment group create --template-file azuredeploy.json --parameters azuredeploy.parameters.json --name $deploymentName
-
-
-# Get Azure Storage Private Endpoint data
-echo "Getting Azure Storage queue private endpoint . . ."
-storagePrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateStorageQueueEndpointNetworkInterface.value --output tsv)
-echo "Private storage queue NIC is $storagePrivateNic"
-
-if [[ ! -z $storagePrivateNic ]]; then
-
-    storageQueuePrivateDnsZoneName=$(az deployment group show --name $deploymentName --query properties.outputs.storageQueuePrivateDnsZoneName.value --output tsv)
-
-    if [[ ! -z $storageQueuePrivateDnsZoneName ]]; then
-
-        az deployment group create --template-file PrivateLinkIPConfigParser.json --parameters deploymentNameSuffix=$now nicId=$storagePrivateNic privateDnsZoneName=$storageQueuePrivateDnsZoneName dnsEntriesHandlerTemplateUri=$dnsEntriesHandlerTemplateUri dnsEntriesTemplateUri=$dnsEntriesTemplateUri --name "PrivateLinkIpConfig-Storage-Queue-$now"
-    fi
-fi
-
-# ----- Begin web jobs storage config -----
-
-echo "Getting Azure web jobs storage queue private endpoint . . ."
-webJobsStorageQueuePrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateEndpointWebJobsQueueStorageNameNetworkInterface.value --output tsv)
-echo "Private web jobs storage queue NIC is $webJobsStorageQueuePrivateNic"
-
-if [[ ! -z $webJobsStorageQueuePrivateNic ]]; then
-
-    if [[ ! -z $storageQueuePrivateDnsZoneName ]]; then
-
-        az deployment group create --template-file PrivateLinkIPConfigParser.json --parameters deploymentNameSuffix=$now nicId=$webJobsStorageQueuePrivateNic privateDnsZoneName=$storageQueuePrivateDnsZoneName dnsEntriesHandlerTemplateUri=$dnsEntriesHandlerTemplateUri dnsEntriesTemplateUri=$dnsEntriesTemplateUri --name "PrivateLinkIpConfig-WebJobsStorage-Queue-$now"
-    fi
-fi
-
-echo "Getting Azure web jobs storage table private endpoint . . ."
-webJobsStorageTablePrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateEndpointWebJobsTableStorageNameNetworkInterface.value --output tsv)
-echo "Private web jobs storage table NIC is $webJobsStorageTablePrivateNic"
-
-if [[ ! -z $webJobsStorageTablePrivateNic ]]; then
-    storageTablePrivateDnsZoneName=$(az deployment group show --name $deploymentName --query properties.outputs.storageTablePrivateDnsZoneName.value --output tsv)
-
-    if [[ ! -z $storageTablePrivateDnsZoneName ]]; then
-
-        az deployment group create --template-file PrivateLinkIPConfigParser.json --parameters deploymentNameSuffix=$now nicId=$webJobsStorageTablePrivateNic privateDnsZoneName=$storageTablePrivateDnsZoneName dnsEntriesHandlerTemplateUri=$dnsEntriesHandlerTemplateUri dnsEntriesTemplateUri=$dnsEntriesTemplateUri --name "PrivateLinkIpConfig-WebJobsStorage-Table-$now"
-    fi
-fi
-
-echo "Getting Azure web jobs storage blob private endpoint . . ."
-webJobsStorageBlobPrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateEndpointWebJobsBlobStorageNameNetworkInterface.value --output tsv)
-echo "Private web jobs storage blob NIC is $webJobsStorageBlobPrivateNic"
-
-if [[ ! -z $webJobsStorageBlobPrivateNic ]]; then
-    storageBlobPrivateDnsZoneName=$(az deployment group show --name $deploymentName --query properties.outputs.storageBlobPrivateDnsZoneName.value --output tsv)
-
-    if [[ ! -z $storageBlobPrivateDnsZoneName ]]; then
-
-        az deployment group create --template-file PrivateLinkIPConfigParser.json --parameters deploymentNameSuffix=$now nicId=$webJobsStorageBlobPrivateNic privateDnsZoneName=$storageBlobPrivateDnsZoneName dnsEntriesHandlerTemplateUri=$dnsEntriesHandlerTemplateUri dnsEntriesTemplateUri=$dnsEntriesTemplateUri --name "PrivateLinkIpConfig-WebJobsStorage-Blob-$now"
-    fi
-fi
-
-echo "Getting Azure web jobs storage file private endpoint . . ."
-webJobsStorageFilePrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateEndpointWebJobsFileStorageNameNetworkInterface.value --output tsv)
-echo "Private web jobs storage file NIC is $webJobsStorageFilePrivateNic"
-
-if [[ ! -z $webJobsStorageFilePrivateNic ]]; then
-    storageFilePrivateDnsZoneName=$(az deployment group show --name $deploymentName --query properties.outputs.storageFilePrivateDnsZoneName.value --output tsv)
-
-    if [[ ! -z $storageFilePrivateDnsZoneName ]]; then
-
-        az deployment group create --template-file PrivateLinkIPConfigParser.json --parameters deploymentNameSuffix=$now nicId=$webJobsStorageFilePrivateNic privateDnsZoneName=$storageFilePrivateDnsZoneName dnsEntriesHandlerTemplateUri=$dnsEntriesHandlerTemplateUri dnsEntriesTemplateUri=$dnsEntriesTemplateUri --name "PrivateLinkIpConfig-WebJobsStorage-File-$now"
-    fi
-fi
-
-# --- End web jobs storage config
-
-
-# Get Azure Cosmos DB Private Endpoint data
-echo "Getting CosmosDB private endpoint . . ."
-cosmosDbPrivateNic=$(az deployment group show --name $deploymentName --query properties.outputs.privateEndpointCosmosDbNetworkInterface.value --output tsv)
-echo "CosmosDB private NIC is" $cosmosDbPrivateNic
-
-if [[ ! -z $cosmosDbPrivateNic ]]; then
-
-    echo "Getting CosmosDB private DNS record . . ."
-    cosmosDbPrivateDnsZoneName=$(az deployment group show --name $deploymentName --query properties.outputs.privateCosmosDbDnsZoneName.value --output tsv)
-
-    if [[ ! -z $cosmosDbPrivateDnsZoneName ]]; then
-
-        az deployment group create --template-file PrivateLinkIPConfigParser.json --parameters  deploymentNameSuffix=$now nicId=$cosmosDbPrivateNic privateDnsZoneName=$cosmosDbPrivateDnsZoneName dnsEntriesHandlerTemplateUri=$dnsEntriesHandlerTemplateUri dnsEntriesTemplateUri=$dnsEntriesTemplateUri --name "PrivateLinkIpConfig-CosmosDb-$now"
-    fi
-
-fi

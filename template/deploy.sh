63ff9c83e82181f56517a6b3d9691e5404722627
Michael Collier <mcollier@microsoft.com>
2020-05-07 17:10:01 -0400

    Make vnet addresses parameters; shorten resource name; Remove output values.

--- template/deploy.sh
+++ template/deploy.sh
@@ -4,21 +4,16 @@
 #az account set -s 
 
 
-resourceGroupName="function-private-storage-1002"
+resourceGroupName="functions-private-endpoints-sample-4"
 location="southcentralus"
 now=`date +%Y%m%d-%H%M%S`
 deploymentName="azuredeploy-$now"
-dnsEntriesHandlerTemplateUri="https://raw.githubusercontent.com/mcollier/azure-functions-private-storage/master/template/PrivateLinkDnsEntriesHandler.json"
-dnsEntriesTemplateUri="https://raw.githubusercontent.com/mcollier/azure-functions-private-storage/master/template/PrivateLinkDnsEntries.json"
-
 
 echo "Creating resource group '$resourceGroupName' in region '$location' . . ."
 az group create --name $resourceGroupName --location southcentralus
 
-echo "Setting defaults ...."
-az configure --defaults group=$resourceGroupName location=$location
-
-# az deployment group validate --template-file azuredeploy.json --parameters azuredeploy.parameters.json --debug
+# echo "Validating the template . . ."
+# az deployment group validate -g $resourceGroupName --template-file azuredeploy.json --parameters azuredeploy.parameters.json --debug
 
 echo "Deploying main template . . ."
-az deployment group create --template-file azuredeploy.json --parameters azuredeploy.parameters.json --name $deploymentName
+az deployment group create -g $resourceGroupName --template-file azuredeploy.json --parameters azuredeploy.parameters.json --name $deploymentName
